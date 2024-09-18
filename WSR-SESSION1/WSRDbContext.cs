13ff4678eff9aebf1bfadc1f159342cb35715def
Найт <rodionvarzymov@outlook.com>
2024-09-18 11:49:22 +0300

    Добавлено подключение к БД

---
+++ WSR-SESSION1/WSRDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WSR_SESSION1.Models;
+
+namespace WSR_SESSION1
+{
+    public class WSRDbContext(DbSet<Patient> patients, DbSet<Hospitalization> hospitalizations) : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; } = patients;
+        public DbSet<Hospitalization> Hospitalizations { get; set; } = hospitalizations;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка моделей, если необходимо
+        }
+    }
+}
