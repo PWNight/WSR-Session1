dbb031ecb868a5b9a622601ba64f45139c4fd36c
Найт <rodionvarzymov@outlook.com>
2024-09-18 11:02:40 +0300

    Добавьте файлы проекта.

---
+++ WSR-SESSION1/Models/Hospitalization.cs
@@ -0,0 +1,15 @@
+﻿namespace WSR_SESSION1.Models
+{
+    public class Hospitalization
+    {
+        public required int Id { get; set; }
+        public required int PatientId { get; set; }
+        public required string Code { get; set; }
+        public required DateTime Date { get; set; }
+        public required string Department { get; set; }
+        public required string Conditions { get; set; }
+        public required string AdditionalInfo { get; set; }
+        public required bool IsCancelled { get; set; }
+        public required string CancellationReason { get; set; }
+    }
+}
---
+++ WSR-SESSION1/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace WSR_SESSION1.Models
+{
+    public class Patient
+    {
+        public required int Id { get; set; }
+        public required string Fio { get; set; }
+        public required string PassportData { get; set; }
+        public required string WorkPlace { get; set; }
+        public required string PolicyNumber { get; set; }
+        public required DateTime PolicyExpire { get; set; }
+        public required string PoliceCompany { get; set; }
+        public required string MedicalCardId { get; set; }
+        public required string PhotoPath { get; set; }
+    }
+}
