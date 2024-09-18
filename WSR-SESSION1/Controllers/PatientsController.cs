d8c610387eba47b0591368efa5845a741c4ab91f
Найт <rodionvarzymov@outlook.com>
2024-09-18 11:49:33 +0300

    Добавлен контроллер пациентов

---
+++ WSR-SESSION1/Controllers/PatientsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WSR_SESSION1.Models;
+
+namespace WSR_SESSION1.Controllers
+{
+    public class PatientsController(WSRDbContext context) : Controller
+    {
+        private readonly WSRDbContext _context = context;
+
+        // GET: Patients
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Patients.ToListAsync());
+        }
+
+        // GET: Patients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fio,PassportData,WorkPlace,PolicyNumber,PolicyExpire,PoliceCompany,MedicalCardId,PhotoPath")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fio,PassportData,WorkPlace,PolicyNumber,PolicyExpire,PoliceCompany,MedicalCardId,PhotoPath")] Patient patient)
+        {
+            if (id != patient.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PatientExists(patient.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient != null)
+            {
+                _context.Patients.Remove(patient);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PatientExists(int id)
+        {
+            return _context.Patients.Any(e => e.Id == id);
+        }
+    }
+}
