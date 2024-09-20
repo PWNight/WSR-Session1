30bd9aa0ff14dd89159316d06d97cd249398bc97
Найт <rodionvarzymov@outlook.com>
2024-09-20 15:03:40 +0300

    Обновил класс пациента

--- WSR-SESSION1/Models/Patient.cs
+++ WSR-SESSION1/Models/Patient.cs
@@ -1,30 +1,22 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-namespace WSR_SESSION1.Models
+﻿namespace WSR_SESSION1.Models
 {
     public class Patient
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Fio is required.")]
-        public string Fio { get; set; }
+        public required string Fio { get; set; }
 
-        [Required(ErrorMessage = "PassportData is required.")]
-        public string PassportData { get; set; }
+        public required string PassportData { get; set; }
 
-        [Required(ErrorMessage = "WorkPlace is required.")]
-        public string WorkPlace { get; set; }
+        public required string WorkPlace { get; set; }
 
-        [Required(ErrorMessage = "PolicyNumber is required.")]
-        public string PolicyNumber { get; set; }
+        public required string PolicyNumber { get; set; }
 
-        [Required(ErrorMessage = "PolicyExpire is required.")]
         public DateTime PolicyExpire { get; set; }
 
-        [Required(ErrorMessage = "PolicyCompany is required.")]
-        public string PolicyCompany { get; set; }
+        public required string PolicyCompany { get; set; }
 
         public int MedicalCardId { get; set; }
-        public string PhotoPath { get; set; }
+        public required string PhotoPath { get; set; }
     }
 }
