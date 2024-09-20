13e11df9f8cd4cd1018c28624f89a02a27fe6ac9
Найт <rodionvarzymov@outlook.com>
2024-09-20 15:02:25 +0300

    Удалил излишний функционал

--- WSR-SESSION1/Controllers/PatientsController.cs
+++ WSR-SESSION1/Controllers/PatientsController.cs
@@ -4,37 +4,14 @@ using WSR_SESSION1.Models;
 
 namespace WSR_SESSION1.Controllers
 {
-    public class PatientsController : Controller
+    public class PatientsController(WSRDbContext context) : Controller
     {
-        private readonly WSRDbContext _context;
-
-        public PatientsController(WSRDbContext context)
-        {
-            _context = context;
-        }
+        private readonly WSRDbContext _context = context;
 
         // GET: Patients
         public async Task<IActionResult> Index()
         {
             return View(await _context.Patients.ToListAsync());
-        }
-
-        // GET: Patients/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var patient = await _context.Patients
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (patient == null)
-            {
-                return NotFound();
-            }
-
-            return View(patient);
         }
 
         // GET: Patients/Create
@@ -56,89 +33,6 @@ namespace WSR_SESSION1.Controllers
             }
             return View(patient);
         }
-
-        // GET: Patients/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var patient = await _context.Patients.FindAsync(id);
-            if (patient == null)
-            {
-                return NotFound();
-            }
-            return View(patient);
-        }
-
-        // POST: Patients/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Fio,PassportData,WorkPlace,PolicyNumber,PolicyExpire,PolicyCompany,MedicalCardId,PhotoPath")] Patient patient)
-        {
-            if (id != patient.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(patient);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PatientExists(patient.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(patient);
-        }
-
-        // GET: Patients/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var patient = await _context.Patients
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (patient == null)
-            {
-                return NotFound();
-            }
-
-            return View(patient);
-        }
-
-        // POST: Patients/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var patient = await _context.Patients.FindAsync(id);
-            if (patient != null)
-            {
-                _context.Patients.Remove(patient);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
         private bool PatientExists(int id)
         {
             return _context.Patients.Any(e => e.Id == id);
